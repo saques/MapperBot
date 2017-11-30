28d2b0755a121746041e6b6410c93bb600db87d4
masaques <msaques@itba.edu.ar>
2017-11-30 19:19:08 -0300

    More testing on esp8266

--- TestWifi/TestWifi.ino
+++ TestWifi/TestWifi.ino
@@ -1,9 +1,9 @@
 #include <SoftwareSerial.h>
 #include <SerialESP8266wifi.h>
 
-#define sw_serial_rx_pin 4 //  Connect this pin to TX on the esp8266
-#define sw_serial_tx_pin 6 //  Connect this pin to RX on the esp8266
-#define esp8266_reset_pin 5 // Connect this pin to CH_PD on the esp8266, not reset. (let reset be unconnected)
+#define sw_serial_rx_pin 12 //  Connect this pin to TX on the esp8266
+#define sw_serial_tx_pin 13 //  Connect this pin to RX on the esp8266
+#define esp8266_reset_pin 8 // Connect this pin to CH_PD on the esp8266, not reset. (let reset be unconnected)
 
 SoftwareSerial swSerial(sw_serial_rx_pin, sw_serial_tx_pin);
 
@@ -15,14 +15,14 @@ boolean stringComplete = false;
 
 void setup() {
   inputString.reserve(20);
-  swSerial.begin(115200);
+  swSerial.begin(9600);
   Serial.begin(9600);
   
   while (!Serial)
     ;
   Serial.println("Starting wifi");
 
-  wifi.setTransportToTCP();// this is also default
+  wifi.setTransportToUDP();
 
   wifi.endSendWithNewline(true); // Will end all transmissions with a newline and carrage return ie println.. default is true
 
@@ -30,36 +30,41 @@ void setup() {
 
 
   wifi.connectToAP("Retutatario", "");
-  wifi.connectToServer("192.168.1.101", "2121");
+  wifi.connectToServer("192.168.1.101", "9999");
   Serial.println("Connected");
   wifi.send(SERVER, "ESP8266 test app started");
 }
 
 void loop() {
 
-  //Make sure the esp8266 is started..
-  
   if (!wifi.isStarted())
     wifi.begin();
 
-  
-  //Send what you typed in the arduino console to the server
-  
-  
-  
-  
-  //Listen for incoming messages and echo back, will wait until a message is received, or max 6000ms..
-  WifiMessage in = wifi.getIncomingMessage();
-  if (in.hasData) {
-    Serial.println(in.message);
-    //wifi.send(in.channel, "Echo:", false);
-    wifi.send(in.channel, in.message);
+      //Send what you typed in the arduino console to the server
+  static char buf[20];
+  if (stringComplete) {
+    inputString.toCharArray(buf, sizeof buf);
+    wifi.send(SERVER, buf);
+    inputString = "";
+    stringComplete = false;
   }
-  
 
-  //If you want do disconnect from the server use:
-  // wifi.disconnectFromServer();
+  /*
+  wifi.send(SERVER,"Hola!\r\n");
+  delay(1000);
+  */
 
+}
+
+//Listen for serial input from the console
+void serialEvent() {
+  while (Serial.available()) {
+    char inChar = (char)Serial.read();
+    inputString += inChar;
+    if (inChar == '\n') {
+      stringComplete = true;
+    }
+  }
 }
 
 
