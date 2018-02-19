79c52b8fe15058bbb707ab2f4abcf94b9f4c23f9
masaques <msaques@itba.edu.ar>
2018-02-19 18:07:43 -0300

    arranging final arduino code
    all the code uses 55% of storage space, not bad

--- MapperBot.ino
+++ MapperBot.ino
@@ -1,50 +1,43 @@
 #include <Wire.h>
 #include <time.h>
 #include <stdlib.h>
-
-//wifi
 #include <SoftwareSerial.h>
 #include <SerialESP8266wifi.h>
-#define sw_serial_rx_pin 0 //  Connect this pin to TX on the esp8266
-#define sw_serial_tx_pin 0 //  Connect this pin to RX on the esp8266
-#define esp8266_reset_pin 0 // Connect this pin to CH_PD on the esp8266, not reset. (let reset be unconnected)
-
-//Position
 #include <Position.h>
-#define DELTA_CIRCUMFERENCE 2.0f
-
-//Magnetometer
 #include <QMC5883L.h>
-#define BUENOS_AIRES_DEC -0.13962634f
-
-//Ultrasonic Sensor
 #include <HC_SR04.h>
+#include <HSM5H.h>
+#include <State.h>
+#include <Motor.h>
+#include <Environment.h>
+//wifi
+#define sw_serial_rx_pin A0 
+#define sw_serial_tx_pin A1 
+#define esp8266_reset_pin 8
+//Position
+#define DELTA_CIRCUMFERENCE 2.0f
+//Magnetometer
+#define BUENOS_AIRES_DEC -0.13962634f
+//Ultrasonic Sensor
 #define Trig 7
 #define Echo 8
-
 //Rotary Encoder
-#include <HSM5H.h>
 #define PinA_ROT_ENC 2
 #define PinB_ROT_ENC 3
-
-//State
-#include <State.h>
-
-#include <Environment.h>
-
 //Motors
-#include <Motor.h>
-#define PIN_A_1 0
-#define PIN_B_1 0
-#define PIN_A_2 0
-#define PIN_B_2 0
+#define PIN_A_l 10
+#define PIN_B_l 9
+#define PIN_A_r 6
+#define PIN_B_r 5
 #define MOTOR_SPEED 30
 
 QMC5883L compass;
 Position position;
 HC_SR04 ultrasonic(Trig, Echo);
-Motor m1(PIN_A_1, PIN_B_1, MOTOR_SPEED);
-Motor m2(PIN_A_2, PIN_B_2, MOTOR_SPEED);
+Motor l(PIN_A_l, PIN_B_l, MOTOR_SPEED);
+Motor r(PIN_A_r, PIN_B_r, MOTOR_SPEED);
+SoftwareSerial swSerial(sw_serial_rx_pin, sw_serial_tx_pin);
+SerialESP8266wifi wifi(Serial, Serial, esp8266_reset_pin, swSerial);//adding Serial enabled local echo and wifi debug
 State* state = &Singleton<StraightLine>::getInstance();
 
 /*
@@ -64,15 +57,23 @@ void dec(){
 
 void setup() {
   srand(time(NULL));
-  Wire.begin();
   Serial.begin(9600);
+  swSerial.begin(9600);
+  Wire.begin();
   compass.init();
   initRotaryEncoder(PinA_ROT_ENC, PinB_ROT_ENC, inc, dec);
 
+  //wifi startup
+  wifi.setTransportToUDP(); //Minimizing traffic saves battery (?)
+  wifi.endSendWithNewline(true);
+  wifi.begin();
+  wifi.connectToAP("", "");
+  wifi.connectToServer("192.168.1.110", "9999");
+  wifi.send(SERVER, "MapperBot v1.0");
 
-  Environment::getInstance().setLeftMotor(&m1);
-  Environment::getInstance().setRightMotor(&m2);
-  //Environment::getInstance().setWifi(&wifi);
+  Environment::getInstance().setLeftMotor(&l);
+  Environment::getInstance().setRightMotor(&r);
+  Environment::getInstance().setWiFi(&wifi);
 }
 
 void loop() {
